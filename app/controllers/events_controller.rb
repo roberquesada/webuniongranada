f91b5f08bf23bf949f89f9bb70e9126d638c5600
Roberto Quesada <robertoquesadalizana@gmail.com>
2016-05-03 09:04:54 +0200

    Added layout to event view single

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,8 @@ class EventsController < ApplicationController
   def show
     @event = Event.find_by_slug!(params[:id])
     respond_to do |format|
-      format.html { render layout: false }
       format.json { render json: @event }
+      format.html
     end
   end
 
