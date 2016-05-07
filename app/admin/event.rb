1fc697a42759fb9d530d3430d10ac49201dc71dc
Roberto Quesada <robertoquesadalizana@gmail.com>
2016-05-07 22:20:44 +0200

    Modify ActiveAdmin view event for add category of a event

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,9 +1,20 @@
 ActiveAdmin.register Event do
-permit_params :title, :description, :slug, :type, :date
-before_filter only: [:edit, :destroy, :show, :update] do
-  @event = Event.find_by_slug params[:id]
-end
   permit_params :title, :description, :slug, :category, :date
+  before_filter only: [:edit, :destroy, :show, :update] do
+    @event = Event.find_by_slug params[:id]
+  end
+
+  form do |f|
+    f.inputs "Event Details" do
+      f.input :title
+      f.input :description
+      f.input :slug
+      f.input :category, as: :radio, collection: Event.categories.map { |k,v| [k.capitalize,k] }
+      f.input :date
+    end
+    f.actions
+  end
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
