99d11f16256b429ef1d055ca7b05af0df12fefdb
Roberto Quesada <robertoquesadalizana@gmail.com>
2016-05-07 22:27:28 +0200

    Now can add speakers by ActiveAdmin

--- app/admin/speaker.rb
+++ app/admin/speaker.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Speaker do
 
+permit_params :name, :description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
