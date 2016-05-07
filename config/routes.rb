76e93ce328173d63a164ec1f2b6a74ad0cd3a417
Roberto Quesada <robertoquesadalizana@gmail.com>
2016-05-07 22:50:21 +0200

    Added contact route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/' => 'site#index', as: :root
+  get '/contact' => 'site#contact', path: :contacto
   resources :events, path: :eventos
 end
