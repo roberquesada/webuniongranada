3e89e648372b9e6c16313ca34e17cf5466a32068
Roberto Quesada <robertoquesadalizana@gmail.com>
2016-05-13 18:37:22 +0200

    Added section speaker and about us (WUG)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get '/' => 'site#index', as: :root
   get '/contact' => 'site#contact', path: :contacto
-  resources :events, path: :eventos
+  get '/wug' => 'site#about'
+  resources :events, only: [:index, :show],  path: :eventos
+  resources :speakers, only: [:index, :show],  path: :ponentes
 end
