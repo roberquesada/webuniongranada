1163983535c0c6857e2b9969d771b4bfce5d701c
Roberto Quesada <robertoquesadalizana@gmail.com>
2016-05-13 18:40:52 +0200

    Removed menu names capitalized

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def custom_link name, route_path, controller_name, action_name = nil
       active = "HeaderMenu--active" if controller.controller_name == controller_name && (controller.action_name == action_name||action_name.nil?)
-      link_to name.capitalize, send("#{route_path}_path"), class: "HeaderMenu-itemLink #{active}"
+      link_to name, send("#{route_path}_path"), class: "HeaderMenu-itemLink #{active}"
   end
 
 end
