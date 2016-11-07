bef4efed1b375353fcea5f72b6eea413599ff5b4
Roberto Quesada <robertoquesadalizana@gmail.com>
2016-11-07 17:03:41 +0100

    Fixed issue at site controller when there aren't events

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,6 @@
 class SiteController < ApplicationController
   def index
-    @last_event = Event.limit(1).first
-    @last_event_speakers = @last_event.speakers
+    @last_event = Event.limit(1).try(:first)
+    @last_event_speakers = @last_event.speakers if @last_event
   end
 end
